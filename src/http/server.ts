25dbdf779b7c90e702e9e6fd75d5e8fcc6cab51f
Oziel Sousa <ozielzin358@gmail.com>
2024-09-10 22:50:26 -0300

    feat: adds creation and get goals function

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,40 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-wee-pending-goals'
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+  return { pendingGoals }
+})
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
